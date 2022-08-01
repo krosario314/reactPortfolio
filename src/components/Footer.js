2879dd359a820dc6bae5ab2d8d3c3cc260b3a3df
Kayla Rosario <rosariok314@gmail.com>
2022-08-01 01:49:47 -0400

    contact page and footer file has been modified

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Kayla's Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/krosario314" target="_blank" rel="noreferrer">
+            Kayla M Rosario
+          </a>
+          .
+          <hr />
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
